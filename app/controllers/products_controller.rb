d90648df280e123a824a64b4ec0aa2a4a58828f0
Olli Wirpi <olliwirp@gmail.com>
2016-04-14 22:36:08 +0300

    Initial commit.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,37 @@
+class ProductsController < ApplicationController
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render 'edit'
+    end
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:title, :description)
+  end
+end
